4d5708548655455b856d50ff40b0b22304ced61b
Adam Wróbel <adam@fluxinc.ca>
2011-01-03 22:56:34 +0100

    Use random color from palette.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
+  DefaultColors = ['#fa7b71', '#f6b75c', '#f1de62', '#c0da60', '#71b5fc',
+                   '#cca3de', '#b8b7b8']
+
   has_many :transactions, :inverse_of => :category
 
   validates :name,
@@ -10,13 +13,26 @@ class Category < ActiveRecord::Base
   before_validation :init_color
 
   def self.random_color
-    '#' + (0..2).map { |i| (i*85 + rand(85)).to_s(16).rjust(2, '0') }.join
+    '#' +
+      (0..2).sort_by { rand }
+      .map { |i| (i*85 + rand(85)).to_s(16).rjust(2, '0') }
+      .join
+  end
+
+  def self.random_pallete_color
+    colorUsage = {}
+    DefaultColors.each { |color| colorUsage[color] = 0 }
+    self.all.each { |category|
+      colorUsage[category.color] += 1 if colorUsage.has_key? category.color
+    }
+    timesUsed = colorUsage.values.sort.first
+    colorUsage.select {|color,usage| usage == timesUsed}.keys.sample
   end
 
   protected
   def init_color
     if color.nil?
-      self.color = Category.random_color
+      self.color = Category.random_pallete_color
     end
   end
 end
