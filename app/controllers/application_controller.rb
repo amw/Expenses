b1c9ee4ce56da927f794512a9f98741cf37ca75b
Adam Wróbel <adam@fluxinc.ca>
2011-01-04 19:47:34 +0100

    404 on RecordNotFound.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  protected
+  def render_404
+    render :file => 'public/404.html', :layout => false,
+      :status => :not_found
+  end
 end
